d6d735afb8e6ec920cf3ee1d6aabd27a8b373264
Arun Kumar <aruncode49@gmail.com>
2023-09-15 16:15:12 +0530

    Send message from client to server

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
 const PORT = 8000;
 
+// serve static file
+app.use(express.static("public"));
+
+// create http server
+const httpServer = http.createServer(app);
+
+// socket server instance
+const io = new Server(httpServer);
+
 app.get("/", (req, res) => {
-  res.end("Hello World");
+  res.sendFile("/index.html", (err) => {
+    console.log(`Error inside sendFile: ${err}`);
+  });
 });
 
-app.listen(PORT, () => {
+// make socket connection
+io.on("connection", (socket) => {
+  // recieve message on server from client
+  socket.on("client-message", (message) => {
+    console.log(message);
+  });
+});
+
+// here app.listen is not working because of new httpServer
+httpServer.listen(PORT, () => {
   console.log(`Server is listening on port: ${PORT}`);
 });
